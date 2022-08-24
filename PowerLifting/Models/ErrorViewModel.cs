5f1694e36e129aadf29388b178abf1caff40480f
Михаил <caragus_niveus@mail.ru>
2022-08-24 11:09:38 +0300

    baseline-to clear

---
+++ PowerLifting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PowerLifting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
