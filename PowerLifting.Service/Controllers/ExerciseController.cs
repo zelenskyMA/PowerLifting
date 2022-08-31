569770b52d342db483923097e3f8d1bbe5c9341e
Михаил <caragus_niveus@mail.ru>
2022-08-31 19:00:39 +0300

    заработали контроллеры

--- PowerLifting.Service/Controllers/ExerciseController.cs
+++ PowerLifting.Service/Controllers/ExerciseController.cs
@@ -9,13 +9,13 @@ namespace PowerLifting.Service.Controllers
   public class ExerciseController : ControllerBase
   {
     private readonly IExerciseApp _exerciseApp;
-
     public ExerciseController(IExerciseApp exerciseApp)
     {
       _exerciseApp = exerciseApp;
     }
 
     [HttpGet]
+    [Route("getList")]
     public async Task<List<Exercise>> GetList()
     {
       var result = await _exerciseApp.GetListAsync();
