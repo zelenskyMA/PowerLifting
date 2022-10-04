39060e8c4d0d8db6f3056679ea6a4fb3259e1f16
Михаил <caragus_niveus@mail.ru>
2022-10-04 23:13:29 +0300

    Багофиксы и улучшайзинг

--- PowerLifting.Service/ClientApp/src/components/NavMenu.js
+++ PowerLifting.Service/ClientApp/src/components/NavMenu.js
@@ -35,7 +35,7 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Помощник спортсмена</NavbarBrand>
+          <NavbarBrand tag={Link} to="/">Спортивный ассистент</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
             <ul className="navbar-nav flex-grow">
