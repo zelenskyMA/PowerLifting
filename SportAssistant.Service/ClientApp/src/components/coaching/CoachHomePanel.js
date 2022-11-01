561daade68b0d30b1f224abb49d68dab1506c65f
Михаил <caragus_niveus@mail.ru>
2022-11-01 12:41:38 +0300

    Добавлены две версии меню. Обычная и без тренировок

---
+++ SportAssistant.Service/ClientApp/src/components/coaching/CoachHomePanel.js
@@ -0,0 +1,30 @@
+﻿import React, { Component } from 'react';
+import { LoadingPanel } from "../../common/controls/CustomControls";
+import WithRouter from "../../common/extensions/WithRouter";
+import '../../styling/Common.css';
+
+class CoachHomePanel extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+
+  componentDidMount() { this.getInitData(); }
+
+  getInitData = async () => {    
+    this.setState({ loading: false });
+  }
+ 
+  render() {
+    if (this.state.loading) { return (<LoadingPanel />); }
+  
+    return (
+      <>Экран тренера</>
+    );
+  }
+}
+
+export default WithRouter(CoachHomePanel);
---
+++ SportAssistant.Service/ClientApp/src/components/main/menu/CommonTopMenu.js
@@ -0,0 +1,53 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+import { NavItem, NavLink } from 'reactstrap';
+import '../../../styling/Common.css';
+
+export function CommonTopMenu({ userInfo }) {
+  return (<>
+    {mainMenu(userInfo)}
+    {coachLink(userInfo)}
+    {adminLink(userInfo)}
+  </>);
+}
+
+function mainMenu(userInfo) {
+  var legalName = userInfo?.legalName ?? '';
+  if (legalName === '') { return (<></>); }
+
+  return (
+    <>
+      <NavItem>
+        <NavLink tag={Link} className="text-dark" to="/plansList">Планы</NavLink>
+      </NavItem>
+
+      <NavItem>
+        <NavLink tag={Link} className="text-dark" to="/exercises">Упражнения</NavLink>
+      </NavItem>
+
+      <NavItem className="spaceRight">
+        <NavLink tag={Link} className="text-dark" to="/planAnalitics">Аналитика</NavLink>
+      </NavItem>
+    </>
+  );
+}
+
+function adminLink(userInfo) {
+  if (!userInfo?.rolesInfo?.isAdmin) { return (<></>) }
+
+  return (
+    <NavItem style={{ marginRight: '20px' }}>
+      <NavLink tag={Link} className="text-dark" to="/adminConsole">Администрирование</NavLink>
+    </NavItem>
+  );
+}
+
+function coachLink(userInfo) {
+  if (!userInfo?.rolesInfo?.isCoach) { return (<></>) }
+
+  return (
+    <NavItem style={{ marginRight: '20px' }}>
+      <NavLink tag={Link} className="text-dark" to="/coachConsole">Тренерская</NavLink>
+    </NavItem>
+  );
+}
